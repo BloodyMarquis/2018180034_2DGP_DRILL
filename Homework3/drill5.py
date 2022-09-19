c33e0266e1a67372845aa8a718f6454feb2b6144
Rudy <blimeing1@tukorea.ac.kr>
2022-09-19 22:58:39 +0900

    drill5.py added

---
+++ Homework3/drill5.py
@@ -0,0 +1,31 @@
+import turtle
+
+def move_forward():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+def move_back():
+    turtle.setheading(-90)
+    turtle.forward(50)
+    turtle.stamp()
+def move_left():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+def move_right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+def restart():
+    turtle.reset()
+    turtle.stamp()
+
+turtle.shape("turtle")
+turtle.stamp()
+
+turtle.onkey(move_forward, 'w')
+turtle.onkey(move_back, 's')
+turtle.onkey(move_left, 'a')
+turtle.onkey(move_right, 'd')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
